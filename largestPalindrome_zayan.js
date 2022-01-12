157c97c8aacda31f3658e9e7d54151359953b4fe
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2022-01-12 10:39:13 -0700

    dead x_x

--- largestPalindrome_zayan.js
+++ largestPalindrome_zayan.js
@@ -8,33 +8,29 @@
 //  0. Obtain this file from https://github.com/ankur-cp/largest-palindrome-solution
 //  1. Rename this file, substituting "yourname" with your name
 //  2. Replace "yourname" with your name in the variable below
-exports.name = "yourname";
+exports.name = "Zayan, Jonathan";
 //  3. Add your optimizations to the solution below
 //  4. Submit a pull request
-
 
 isPalindrome = (num) => {
   // determine if input value matches it's reversed value (i.e. check if it's a palindrome!)
   strNum = num.toString();
   return strNum === strNum.split("").reverse().join("");
-}
-
+};
 
 exports.getLargestPalindrome = (N) => {
-
   let largestPalindrome = 0;
 
   // iterate through range of multiplicands
   for (let i = 10 ** (N - 1); i < 10 ** N; i++) {
-      
+    jCheck(i);
+  }
+
+  function jCheck(i) {
     for (let j = 10 ** (N - 1); j < 10 ** N; j++) {
-
-      // check if the product is a palindrome
       if (isPalindrome(i * j)) {
-
         // check if it's larger than our largest palindrome
         if (i * j > largestPalindrome) {
-                  
           // update largest palindrome
           largestPalindrome = i * j;
         }
@@ -44,7 +40,7 @@ exports.getLargestPalindrome = (N) => {
 
   // return result
   return largestPalindrome;
-}
+};
 
 // input
 const N = 3;
@@ -58,8 +54,7 @@ const N = 3;
 // N=5, correct output = 9966006699
 // N=6, correct output = 999000000999
 // N=7, correct output = 99956644665999
-
 // execution
-console.time(exports.name)
-console.log("result:", exports.getLargestPalindrome(N))
-console.timeEnd(exports.name)
+console.time(exports.name);
+console.log("result:", exports.getLargestPalindrome(N));
+console.timeEnd(exports.name);
